37e5b06708b4f7fd45792f7bed4fcbb88c6c8b5e
DemNeg <basingoudembele@gmail.com>
2022-04-14 18:44:41 +0200

    add class-validator, class-transformer

--- src/personne/controllers/personne.controller.ts
+++ src/personne/controllers/personne.controller.ts
@@ -1,5 +1,5 @@
 /* eslint-disable prettier/prettier */
-import { Body, Controller, Delete, Get, Param, Patch, Post, Put, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, Put, Query } from '@nestjs/common';
 import { PaginationDto } from '../dtos/pagination_dto';
 import { PersonneAddDto } from '../dtos/personne_add.dto';
 import { PersonneEditDto } from '../dtos/personne_edit.dto';
@@ -19,9 +19,9 @@ export class PersonneController {
 
     // Action pour retourner une seule personne
     @Get(':id')
-    getOnePersonne(@Param('id')personneId)
+    getOnePersonne(@Param('id',ParseIntPipe)personneId)
     {
-        return this.personneService.getOnePersonne(+personneId);
+        return this.personneService.getOnePersonne(personneId);
     }
 
     // Action pour ajouter une seule personne
@@ -32,20 +32,20 @@ export class PersonneController {
 
     // Action pour mettre à jour une  personne
     @Put('miseAjour/:id')
-    miseAjourPersonne(@Param('id')personneId,@Body()newPersonne:PersonneEditDto){
-        return this.personneService.miseAjourPersonne(+personneId,newPersonne);
+    miseAjourPersonne(@Param('id',ParseIntPipe)personneId,@Body()newPersonne:PersonneEditDto){
+        return this.personneService.miseAjourPersonne(personneId,newPersonne);
     }
 
     // Action pour mettre à jour partiellement une  personne
     @Patch('editPartielle/:id')
-    editPartiellePersonne(@Param('id')personneId,@Body()newPersonne:Partial<PersonneEditDto>){
-       return this.personneService.editPartiellePersonne(+personneId,newPersonne)
+    editPartiellePersonne(@Param('id',ParseIntPipe)personneId,@Body()newPersonne:Partial<PersonneEditDto>){
+       return this.personneService.editPartiellePersonne(personneId,newPersonne)
     }
 
     // Action pour supprimer une  personne
     @Delete('remove/:id')
-    deletePersonne(@Param('id')personneId){
-        return this.personneService.deletePersonne(+personneId)
+    deletePersonne(@Param('id',ParseIntPipe)personneId){
+        return this.personneService.deletePersonne(personneId)
     }
 
 }
